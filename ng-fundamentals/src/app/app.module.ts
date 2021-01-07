648a5f6b8e94d2c1fb461ff2147bdbfc731b0738
jason-nordheim <jason.nordheim@gmail.com>
2021-01-07 11:24:20 -0700

    added declaration for NavBarComponent

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EventListComponent } from './events/list.component';
 import { EventThumbnailComponent } from './events/thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent, EventListComponent, EventThumbnailComponent],
+  declarations: [
+    AppComponent,
+    EventListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
